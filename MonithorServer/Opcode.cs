f37c459ab1063cde41afe3f1301c8242b4417be0
Zacharias Berggren <zache94@hotmail.com>
2016-02-04 13:36:49 +0100

    Change project name and added opcodes

---
+++ MonithorServer/Opcode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonithorServer
+{
+    class Opcode
+    {
+        enum Code
+        {
+
+            //Arma 3 --> this server
+            SETUP = 0, //Request setup
+            STATUS = 1, //Arma 3 server status
+            PLAYERS = 2, //Arma 3 server players
+
+
+           // this server --> Arma 3
+           ACCSETUP = 3, //Accept setup
+           COM = 4,  //Send command
+
+
+        }
+    }
+}
